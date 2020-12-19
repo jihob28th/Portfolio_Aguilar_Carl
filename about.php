40a33c5f07349ee175a6c4d9fd74636281ead639
jihob28th <60122546+jihob28th@users.noreply.github.com>
2020-12-19 22:52:18 +0800

    Update about.php

--- about.php
+++ about.php
@@ -3,25 +3,21 @@
         <title>About Me</title>
         <link rel="stylesheet" type="text/css" href="styleAbout.css">
     </head>
-    <body>
-        <div class="container">
-            <div class="sky"></div></br></br>
-            <div class="grass"></div>
-            <center><h1>ABOUT ME</h1><br/>
+    <body style="background-color: #91c9ed"><br/>
+            <h1>ABOUT ME</h1><br/>
                     <h3>Name: Carl Jerome S. Aguilar<br/>
                     Age: 21<br/>
                     Profession: Student<br/>
                     Course: Bachelor of Science Major in Information and Technology<br/>
                     Skills: CSS, SVG, Canvas, HTML and PHP<br/></h3>
-            </center>
             <object data="svgIndex/sun.svg" type="image/svg+xml" class="sun"></object>
             <object data="svgIndex/cloud.svg" type="image/svg+xml" class="cloud01"></object>
             <object data="svgIndex/cloud.svg" type="image/svg+xml" class="cloud02"></object>
+            <div class="grass"></div>
             <object data="svgIndex/bushdark.svg" type="image/svg+xml" class="bushdark01"></object>
             <object data="svgIndex/bushdark.svg" type="image/svg+xml" class="bushdark02"></object>
             <object data="svgIndex/bushdark.svg" type="image/svg+xml" class="bushdark03"></object>
             <object data="svgIndex/bushdark.svg" type="image/svg+xml" class="bushdark04"></object>
             <object data="svgIndex/bushdark.svg" type="image/svg+xml" class="bushdark05"></object>
-        </div>
     </body>
 </html>
